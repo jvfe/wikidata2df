f003352849eef9b7d7b96e8631e214232e9cf507
jvfe <jvfecav@gmail.com>
2020-08-08 16:19:56 -0300

    chore: Update development configs / enviroment
    Changing a make command to deal properly with the venv, updating the dev
    enviroment, adding tags and dependencies for the setup.py and changing its entrypoint.

--- setup.py
+++ setup.py
@@ -4,50 +4,50 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = ["requests", "pandas"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="João Vitor F. Cavalcante",
-    author_email='jvfe@ufrn.edu.br',
-    python_requires='>=3.5',
+    author_email="jvfe@ufrn.edu.br",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Utility package for easily turning a SPARQL query into a dataframe",
-    entry_points={
-        'console_scripts': [
-            'wikidata2df=wikidata2df.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["wikidata2csv=wikidata2df.cli:main",],},
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='wikidata2df',
-    name='wikidata2df',
-    packages=find_packages(include=['wikidata2df', 'wikidata2df.*']),
+    keywords="wikidata2df wikidata open-data open data utility",
+    name="wikidata2df",
+    packages=find_packages(include=["wikidata2df", "wikidata2df.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/jvfe/wikidata2df',
-    version='0.1.0',
+    url="https://github.com/jvfe/wikidata2df",
+    version="0.1.0",
     zip_safe=False,
 )
